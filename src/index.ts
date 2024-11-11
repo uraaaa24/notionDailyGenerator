bdf795d7d0b2a35ca7b72e8854f25e82c9c96e36
Yuto Urashima <yuto.urashima24@gmail.com>
2024-11-11 09:15:44 +0900

    fix: correct date formatting and update locale in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,21 @@
 import { Client } from '@notionhq/client'
 import dayjs from 'dayjs'
-import ja from 'dayjs/locale/ja'
+import en from 'dayjs/locale/ja'
 import timezone from 'dayjs/plugin/timezone'
 import utc from 'dayjs/plugin/utc'
 
 dayjs.extend(utc)
 dayjs.extend(timezone)
-dayjs.locale(ja)
+dayjs.locale(en)
 
 const DATABASE_ID = process.env.NOTION_DATABASE_ID || ''
-
 const notion = new Client({ auth: process.env.NOTION_API_KEY })
-
 const now = dayjs().tz('Asia/Tokyo')
-const YYYYMMDD = now.format('YYYY-MM-DD')
-const YYYYMMDDdd = now.format('YYYY-MM-DD (ddd)')
+const YYYYMMDD = now.format('DD-MM-YYYY')
+const YYYYMMDDdd = now.format('DD-MM-YYYY (ddd)')
 
-const createDailyPage = async () => {
-  const alreadyExist = await notion.databases.query({
+const dailyPageExists = async () => {
+  const res = await notion.databases.query({
     database_id: DATABASE_ID,
     filter: {
       property: '日付',
@@ -26,8 +24,11 @@ const createDailyPage = async () => {
       }
     }
   })
+  return res.results.length > 0
+}
 
-  if (alreadyExist.results.length > 0) {
+const createDailyPage = async () => {
+  if (dailyPageExists()) {
     console.log('Already exist')
     return
   }
